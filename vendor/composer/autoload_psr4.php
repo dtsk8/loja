d54426d486336a18c5b91e65b3111f70c5f0c868
Danilo Turqueti <danilo.turquet@gmail.com>
2018-03-04 13:22:31 -0300

    classes

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'hcodebr' => array($vendorDir . '/hcodebr/php-classes/src'),
+    'DTS\\' => array($vendorDir . '/hcodebr/php-classes/src'),
 );
