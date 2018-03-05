a7286d7e4ff06ab0929fbdada6b4454bc7844dc3
Danilo Turqueti <danilo.turquet@gmail.com>
2018-03-05 17:00:45 -0300

    Page

--- index.php
+++ index.php
@@ -2,16 +2,28 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \DTS\Page;
+
+//$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
+	/*
 	$sql = new DTS\DB\Sql();
 	$results = $sql->select("SELECT * FROM tb_users");
 
 	echo json_encode($results);
+	*/
+	$page = new Page();
+
+	$page->setTpl("index");
+
+	$page->setTpl("footer");
+
 
 });
 
