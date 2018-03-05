ab497aa203e5f55d63a46d634f67318dffa6229b
Danilo Turqueti <danilo.turquet@gmail.com>
2018-03-05 17:46:24 -0300

    TPL ADMIN

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \DTS\Page;
+use \DTS\PageAdmin;
 
 //$app = new \Slim\Slim();
 $app = new Slim();
@@ -19,6 +20,24 @@ $app->get('/', function() {
 	echo json_encode($results);
 	*/
 	$page = new Page();
+
+	$page->setTpl("index");
+
+	$page->setTpl("footer");
+
+
+});
+
+
+$app->get('/admin', function() {
+    
+	/*
+	$sql = new DTS\DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
+	*/
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
